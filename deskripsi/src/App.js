35b5cce373221e6b7ea33c023cc0d07103c13ace
kevin <93351620+kevin24102002@users.noreply.github.com>
2023-10-21 22:43:58 +0700

    deskripsi 71210797

---
+++ deskripsi/src/App.js
@@ -0,0 +1,13 @@
+import Deskripsifilm from "./Components/Deskripsi";
+import Navigasi from "./Components/Navigation";
+
+function App() {
+  return (
+    <div>
+     <Navigasi></Navigasi>
+     <Deskripsifilm></Deskripsifilm>
+    </div>
+  );
+}
+
+export default App;
