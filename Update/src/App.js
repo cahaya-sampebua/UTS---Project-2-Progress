98364c3da578a9f3740062cb6521b43c8181e6e7
cahaya-sampebua <93310826+cahaya-sampebua@users.noreply.github.com>
2023-10-23 02:52:06 +0700

    71210695 Update

---
+++ Update/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Update from './components/Update';
+import './components/Update.css'; 
+
+
+function App() {
+  return (
+    <div className="App">
+      <Update />
+    </div>
+  );
+}
+
+export default App;
