946e5b0a60ffa961ed63bc5c5c19d034dc719e7e
cahaya-sampebua <93310826+cahaya-sampebua@users.noreply.github.com>
2023-10-23 09:15:00 +0700

    71210695 Create

--- Create/src/components/Create.js
+++ Create/src/components/Create.js
@@ -1,17 +1,26 @@
-import React, { useState } from 'react';
-import { Form, Button, FormGroup, Label, Input } from 'reactstrap';
-import './Create.css';
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import FormGroup from "react-bootstrap/FormGroup";
+import FormLabel from "react-bootstrap/FormLabel";
+import FormControl from "react-bootstrap/FormControl";
+import FormCheck from "react-bootstrap/FormCheck";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import "./Create.css";
 
 const Create = () => {
-  const [name, setName] = useState('');
-  const [country, setCountry] = useState('');
-  const [director, setDirector] = useState('');
-  const [rating, setRating] = useState('');
-  const [cast, setCast] = useState('');
-  const [releaseDate, setReleaseDate] = useState('');
-  const [duration, setDuration] = useState('');
-  const [synopsis, setSynopsis] = useState('');
-  const [image, setImage] = useState('');
+  const [name, setName] = useState("");
+  const [country, setCountry] = useState("");
+  const [director, setDirector] = useState("");
+  const [rating, setRating] = useState("");
+  const [cast, setCast] = useState("");
+  const [releaseDate, setReleaseDate] = useState("");
+  const [duration, setDuration] = useState("");
+  const [synopsis, setSynopsis] = useState("");
+  const [image, setImage] = useState("");
   const [genre, setGenre] = useState([]);
 
   const handleGenreChange = (e) => {
@@ -29,153 +38,159 @@ const Create = () => {
 
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container">
-          <a className="navbar-brand" href="/">Kembali</a>
-          <span className="navbar-text">Update</span>
-          <a className="navbar-brand" href="/">Keluar</a>
-        </div>
-      </nav>
-
-      <Form onSubmit={handleSubmit}>
-        <FormGroup>
-          <Label for="name">Nama Film</Label>
-          <Input
-            type="text"
-            name="name"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label>Genre</Label>
-          <div>
-            <Label check>
-              <Input
-                type="checkbox"
-                value="Fantasy"
-                checked={genre.includes('Fantasy')}
-                onChange={handleGenreChange}
-              />{' '}
-              Fantasy
-            </Label>
-          </div>
-          <div>
-            <Label check>
-              <Input
-                type="checkbox"
-                value="Adventure"
-                checked={genre.includes('Adventure')}
-                onChange={handleGenreChange}
-              />{' '}
-              Adventure
-            </Label>
-          </div>
-          <div>
-            <Label check>
-              <Input
-                type="checkbox"
-                value="Family"
-                checked={genre.includes('Family')}
-                onChange={handleGenreChange}
-              />{' '}
-              Family
-            </Label>
-          </div>
-          <div>
-            <Label check>
-              <Input
-                type="checkbox"
-                value="Horror"
-                checked={genre.includes('Horror')}
-                onChange={handleGenreChange}
-              />{' '}
-              Horror
-            </Label>
-          </div>
-        </FormGroup>
-        <FormGroup>
-          <Label for="country">Negara</Label>
-          <Input
-            type="text"
-            name="country"
-            id="country"
-            value={country}
-            onChange={(e) => setCountry(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="director">Sutradara</Label>
-          <Input
-            type="text"
-            name="director"
-            id="director"
-            value={director}
-            onChange={(e) => setDirector(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="rating">Rating</Label>
-          <Input
-            type="number"
-            name="rating"
-            id="rating"
-            value={rating}
-            onChange={(e) => setRating(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="cast">Bintang Film</Label>
-          <Input
-            type="text"
-            name="cast"
-            id="cast"
-            value={cast}
-            onChange={(e) => setCast(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="releaseDate">Diterbitkan</Label>
-          <Input
-            type="date"
-            name="releaseDate"
-            id="releaseDate"
-            value={releaseDate}
-            onChange={(e) => setReleaseDate(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="duration">Durasi</Label>
-          <Input
-            type="text"
-            name="duration"
-            id="duration"
-            value={duration}
-            onChange={(e) => setDuration(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="synopsis">Sinopsis</Label>
-          <Input
-            type="textarea"
-            name="synopsis"
-            id="synopsis"
-            value={synopsis}
-            onChange={(e) => setSynopsis(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="image">Gambar</Label>
-          <Input
-            type="file"
-            name="image"
-            id="image"
-            onChange={(e) => setImage(e.target.files[0])}
-          />
-        </FormGroup>
-        <Button type="submit">Create</Button>
-      </Form>
+      <Container>
+        <Row>
+          <Col>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+              <div className="container">
+                <a id="back-button" className="navbar-brand" href="/">
+                  Kembali
+                </a>
+                <span className="navbar-text">Create</span>
+                <a id="exit-button" className="navbar-brand" href="/">
+                  Keluar
+                </a>
+              </div>
+            </nav>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form onSubmit={handleSubmit}>
+              <FormGroup className="mb-3">
+                <FormLabel for="name">Nama Film</FormLabel>
+                <FormControl
+                  type="text"
+                  id="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup className="mb-3">
+                <FormLabel>Genre</FormLabel>
+                <div>
+                  <FormCheck>
+                    <FormCheck.Input
+                      type="checkbox"
+                      value="Fantasy"
+                      checked={genre.includes("Fantasy")}
+                      onChange={handleGenreChange}
+                    />
+                    <FormCheck.Label>Fantasy</FormCheck.Label>
+                  </FormCheck>
+                </div>
+                <div>
+                  <FormCheck>
+                    <FormCheck.Input
+                      type="checkbox"
+                      value="Adventure"
+                      checked={genre.includes("Adventure")}
+                      onChange={handleGenreChange}
+                    />
+                    <FormCheck.Label>Adventure</FormCheck.Label>
+                  </FormCheck>
+                </div>
+                <div>
+                  <FormCheck>
+                    <FormCheck.Input
+                      type="checkbox"
+                      value="Family"
+                      checked={genre.includes("Family")}
+                      onChange={handleGenreChange}
+                    />
+                    <FormCheck.Label>Family</FormCheck.Label>
+                  </FormCheck>
+                </div>
+                <div>
+                  <FormCheck>
+                    <FormCheck.Input
+                      type="checkbox"
+                      value="Horror"
+                      checked={genre.includes("Horror")}
+                      onChange={handleGenreChange}
+                    />
+                    <FormCheck.Label>Horror</FormCheck.Label>
+                  </FormCheck>
+                </div>
+              </FormGroup>
+              <FormGroup className="mb-3">
+                <FormLabel for="country">Negara</FormLabel>
+                <FormControl
+                  type="text"
+                  id="country"
+                  value={country}
+                  onChange={(e) => setCountry(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup className="mb-3">
+                <FormLabel for="director">Sutradara</FormLabel>
+                <FormControl
+                  type="text"
+                  id="director"
+                  value={director}
+                  onChange={(e) => setDirector(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup className="mb-3">
+                <FormLabel for="rating">Rating</FormLabel>
+                <FormControl
+                  type="number"
+                  id="rating"
+                  value={rating}
+                  onChange={(e) => setRating(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup className="mb-3">
+                <FormLabel for="cast">Bintang Film</FormLabel>
+                <FormControl
+                  type="text"
+                  id="cast"
+                  value={cast}
+                  onChange={(e) => setCast(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup className="mb-3">
+                <FormLabel for="releaseDate">Diterbitkan</FormLabel>
+                <FormControl
+                  type="date"
+                  id="releaseDate"
+                  value={releaseDate}
+                  onChange={(e) => setReleaseDate(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup className="mb-3">
+                <FormLabel for="duration">Durasi</FormLabel>
+                <FormControl
+                  type="text"
+                  id="duration"
+                  value={duration}
+                  onChange={(e) => setDuration(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup className="mb-3">
+                <FormLabel for="synopsis">Sinopsis</FormLabel>
+                <FormControl
+                  as="textarea"
+                  id="synopsis"
+                  value={synopsis}
+                  onChange={(e) => setSynopsis(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup className="mb-3">
+                <FormLabel for="image">Gambar</FormLabel>
+                <FormControl
+                  type="file"
+                  id="image"
+                  onChange={(e) => setImage(e.target.files[0])}
+                />
+              </FormGroup>
+              <Button type="submit" className="mt-3">
+                Create
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
